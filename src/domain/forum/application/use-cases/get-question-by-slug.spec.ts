340a623a13b14cedf6c9c9d7d8e5984993c41339
Vinícius Massari <stonefields11@hotmail.com>
2024-10-02 17:23:09 -0300

    question details added

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,38 +1,35 @@
-import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { makeQuestion } from 'test/factories/make-question'
-import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { QuestionsRepository } from '../repositories/questions-repository';
+import { Either, left, right } from '@/core/either';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { Injectable } from '@nestjs/common';
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details';
 
-let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
-let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let sut: GetQuestionBySlugUseCase
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string;
+}
 
-describe('Get Question By Slug', () => {
-  beforeEach(() => {
-    inMemoryQuestionAttachmentsRepository =
-      new InMemoryQuestionAttachmentsRepository()
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
-      inMemoryQuestionAttachmentsRepository,
-    )
-    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
-  })
+type GetQuestionBySlugUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    question: QuestionDetails;
+  }
+>;
 
-  it('should be able to get a question by slug', async () => {
-    const newQuestion = makeQuestion({
-      slug: Slug.create('example-question'),
-    })
+@Injectable()
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
 
-    await inMemoryQuestionsRepository.create(newQuestion)
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findDetailsBySlug(slug);
 
-    const result = await sut.execute({
-      slug: 'example-question',
-    })
+    if (!question) {
+      return left(new ResourceNotFoundError());
+    }
 
-    expect(result.value).toMatchObject({
-      question: expect.objectContaining({
-        title: newQuestion.title,
-      }),
-    })
-  })
-})
+    return right({
+      question,
+    });
+  }
+}
